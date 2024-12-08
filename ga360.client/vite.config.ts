5f3fb9b67fab52cef0fd55d11e397869fb02a108
Gabriel Garcia <arquitectura@signos.io>
2024-12-08 14:06:54 +0000

    viae.config.ts

--- ga360.client/vite.config.ts
+++ ga360.client/vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
@@ -8,16 +7,15 @@ import child_process from 'child_process';
 import { env } from 'process';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+const baseFolder = env.APPDATA !== undefined && env.APPDATA !== ''
+    ? `${env.APPDATA}/ASP.NET/https`
+    : `${env.HOME}/.aspnet/https`;
 
 const certificateName = "ga360.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+if (env.NODE_ENV !== 'production' && (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath))) {
     if (0 !== child_process.spawnSync('dotnet', [
         'dev-certs',
         'https',
@@ -41,130 +39,120 @@ export default defineConfig({
     },
     resolve: {
         alias: [
-            //{
-            //  find: /^~(.+)/,
-            //  replacement: path.join(process.cwd(), 'node_modules/$1')
-            //},
-            //{
-            //  find: /^src(.+)/,
-            //  replacement: path.join(process.cwd(), 'src/$1')
-            //},
-          ]
+            // Custom alias if needed
+        ]
     },
     server: {
         proxy: {
-            '^/weatherforecast': {
-                target: 'https://localhost:7030/',
-                secure: false
-            },
-            '^/menu': {
-                target: 'https://localhost:7030/',
-                secure: false
-            },
-            '^/api/customer/list': {
-                target,
-                secure: false
-            },
-            '^/api/customer/user': {
-                target,
-                secure: false
-            },
-            '^/api/customer/create': {
-                target,
-                secure: false
-            },
-            '^/api/customer/delete': {
-                target,
-                secure: false
-            },
-            '^/api/customer/update': {
-                target,
-                secure: false
-            },
-             '^/api/customer/get': {
-                target,
-                secure: false
-            },
-            '^/api/customer/get/basic': {
-                target,
-                secure: false
-            },
-            '^/api/customer/updatewithdocuments': {
-                target,
-                secure: false
-            },
-            '^/api/customer/customerwithcoursequalificationrecords': {
-                target,
-                secure: false
-            },
-            '^/api/trainingcentre/list': {
-                target,
-                secure: false
-            },
-            '^/api/ethnicity/list': {
-                target,
-                secure: false
-            },
-            '^/api/skill/list': {
-                target,
-                secure: false
-            },
-            '^/api/country/list': {
-                target,
-                secure: false
-            },
-            '^/api/course': {
-                target,
-                secure: false,
-                changeOrigin: true
-            },
-            '^/api/qualification': {
-                target,
-                secure: false,
-                changeOrigin: true
-            },
-            '^/api/trainingcentre': {
-                target,
-                secure: false,
-                changeOrigin: true
-            },
-            '^/api/customer/customerswithcoursequalificationrecords': {
-                target,
-                secure: false,
-                changeOrigin: true
-            },
-            '^/api/certificate': {
-                target,
-                secure: false,
-                changeOrigin: true
-            },
-            '^/api/dashboard': {
-                target,
-                secure: false,
-                changeOrigin: true
-            },
-            '^/signin-oidc': {
-                target,
-                secure: false
-            },
-            '^/bff/login': {
-                target,
-                secure: false
-            },
-            '^/bff/user': {
-                target,
-                secure: false
-            },
-            '^/signout-callback-oidc': {
-                target,
-                secure: false
-            },
+       
+                '^/menu': {
+                    target: 'https://localhost:7030/',
+                    secure: false
+                },
+                '^/api/customer/list': {
+                    target,
+                    secure: false
+                },
+                '^/api/customer/user': {
+                    target,
+                    secure: false
+                },
+                '^/api/customer/create': {
+                    target,
+                    secure: false
+                },
+                '^/api/customer/delete': {
+                    target,
+                    secure: false
+                },
+                '^/api/customer/update': {
+                    target,
+                    secure: false
+                },
+                '^/api/customer/get': {
+                    target,
+                    secure: false
+                },
+                '^/api/customer/get/basic': {
+                    target,
+                    secure: false
+                },
+                '^/api/customer/updatewithdocuments': {
+                    target,
+                    secure: false
+                },
+                '^/api/customer/customerwithcoursequalificationrecords': {
+                    target,
+                    secure: false
+                },
+                '^/api/trainingcentre/list': {
+                    target,
+                    secure: false
+                },
+                '^/api/ethnicity/list': {
+                    target,
+                    secure: false
+                },
+                '^/api/skill/list': {
+                    target,
+                    secure: false
+                },
+                '^/api/country/list': {
+                    target,
+                    secure: false
+                },
+                '^/api/course': {
+                    target,
+                    secure: false,
+                    changeOrigin: true
+                },
+                '^/api/qualification': {
+                    target,
+                    secure: false,
+                    changeOrigin: true
+                },
+                '^/api/trainingcentre': {
+                    target,
+                    secure: false,
+                    changeOrigin: true
+                },
+                '^/api/customer/customerswithcoursequalificationrecords': {
+                    target,
+                    secure: false,
+                    changeOrigin: true
+                },
+                '^/api/certificate': {
+                    target,
+                    secure: false,
+                    changeOrigin: true
+                },
+                '^/api/dashboard': {
+                    target,
+                    secure: false,
+                    changeOrigin: true
+                },
+                '^/signin-oidc': {
+                    target,
+                    secure: false
+                },
+                '^/bff/login': {
+                    target,
+                    secure: false
+                },
+                '^/bff/user': {
+                    target,
+                    secure: false
+                },
+                '^/signout-callback-oidc': {
+                    target,
+                    secure: false
+                },
+            
         },
         port: 5173,
-        https: {
+        https: env.NODE_ENV !== 'production' ? {
             key: fs.readFileSync(keyFilePath),
             cert: fs.readFileSync(certFilePath),
-        }
+        } : undefined
     }
-})
-
+});
